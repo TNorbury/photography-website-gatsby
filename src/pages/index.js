506aa23bfb91fe558da7ed98157b2bdcaf3d7454
Tyler Norbury <tylernorbury96@gmail.com>
2019-08-21 21:22:46 -0700

    basic carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,81 @@
 import React from 'react';
 import Layout from '../components/layout';
 import AlbumCard from '../components/albumCard';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { Container, Row } from 'reactstrap';
-import { css } from '@emotion/core';
+import {
+    CarouselProvider,
+    Slider,
+    Slide,
+    ButtonBack,
+    ButtonNext,
+    Image,
+    DotGroup,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
-export default ({ data }) => {
-    return (
-        <Layout>
-            <Container
-                css={css`
-                    margin-right: auto;
-                `}
-            >
-                <Row>
-                    {data.allAlbumsJson.edges.map(({ node }) => (
-                        <AlbumCard
-                            title={node.title}
-                            thumbnail={node.thumbnail.childImageSharp.fixed.src}
-                            
-                            // replace spaces w/ -
-                            link={
-                                '/' +
-                                node.title.toLowerCase().replace(/ /g, '-').replace(/ü/g, "u")
-                            }
-                        />
-                    ))}
-                </Row>
-            </Container>
-        </Layout>
-    );
-};
+import Img from 'gatsby-image';
+import { css } from '@emotion/core';
+import '../styles/home.css'
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <Container>
+                    <CarouselProvider
+                        naturalSlideWidth={20}
+                        naturalSlideHeight={14}
+                        totalSlides={this.props.data.allAlbumsJson.edges.length}
+                    >
+                        <Slider>
+                            {/* <Slide index={0}>I am the first Slide.</Slide>
+                            <Slide index={1}>I am the second Slide.</Slide>
+                            <Slide index={2}>I am the third Slide.</Slide> */}
+                            {this.props.data.allAlbumsJson.edges.map(
+                                ({ node }, index) => (
+                                    <Slide index={index}>
+                                        <Link
+                                            to={
+                                                '/' +
+                                                node.title
+                                                    .toLowerCase()
+                                                    .replace(/ /g, '-')
+                                                    .replace(/ü/g, 'u') +
+                                                '/'
+                                            }
+                                        >
+                                            <Image
+                                                src={
+                                                    node.thumbnail
+                                                        .childImageSharp.fluid
+                                                        .src
+                                                }
+                                            />
+                                            <p
+                                                css={css`
+                                                    position: absolute;
+                                                    top: 8px;
+                                                    left: 16px;
+                                                `}
+                                            >
+                                                {node.title}
+                                            </p>
+                                        </Link>
+                                    </Slide>
+                                )
+                            )}
+                        </Slider>
+                            <ButtonBack css={css`float:left`}>{'<'}</ButtonBack >
+                            {/* <DotGroup css={css`display:inline-block`} /> */}
+
+                            <ButtonNext css={css`display:inline-block`}>{'>'}</ButtonNext>
+                    </CarouselProvider>
+                </Container>
+            </Layout>
+        );
+    }
+}
 
 export const query = graphql`
     query {
@@ -42,8 +87,9 @@ export const query = graphql`
                     parentAlbum
                     thumbnail {
                         childImageSharp {
-                            fixed(width: 300, height: 300, quality: 90) {
+                            fluid(maxWidth: 2000) {
                                 src
+                                aspectRatio
                             }
                         }
                     }
